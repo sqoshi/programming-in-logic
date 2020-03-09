2a86f9c3ea8b3f39540ca355459043467bd47bec
Piotr <piotrpopis98@icloud.com>
2020-03-09 16:51:08 +0100

    missing 4 and 5 in l2

--- 245162/List02/task04/db.pl
+++ 245162/List02/task04/db.pl
@@ -1,40 +1,50 @@
+%0+Y=Y
 regula(X,+,Y,Y) :- %y+0=y
     number(X),
     X=:=0,!.
+%X+0=X
+regula(X,+,Y,X):-
+    number(Y),
+    Y=:=0,!.
 
+%X-0=X
 regula(X,-,Y,X):-
     number(Y),
     Y=:=0,!.
 
+%0-Y=-Y
+regula(X,-,Y,Y):-
+    number(X),
+    X=:=0.
+
+%0*0 = error - false
 regula(0,*,0,_):-
     false.
-
-regula(X,*,X,_):-
-    false,
-    number(X),
-    X =:= 0,
-    !.
-
+% X*0 = 0 i 0*Y =0
 regula(X,*,Y,0):-
     number(X),
     number(Y),
     X\=Y,
     (X is 0;Y is 0),!.
 
-uprosc(expr,out):-
-    regula(
-    .
+%X * 1 = X
 regula(X,*,Y,X):-
     number(Y),
     Y is 1,!.
 
+%1 * Y = Y
 regula(X,*,Y,Y):-
     number(X),
     X is 1,!.
 
+% X/1 = X
 regula(X,/,Y,X):-
     number(Y),
     Y=:=1,!.
+
+% X / 0 - zawsze false!
+regula(_,/,0,_):-
+    false,!.
 
 
 
